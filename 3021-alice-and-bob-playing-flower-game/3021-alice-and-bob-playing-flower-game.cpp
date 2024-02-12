208f72279fe2a8d728c4f97f110b96751522ddd4
Noochila <112234009+Noochila@users.noreply.github.com>
2024-02-12 14:15:46 +0530

    Time: 0 ms (100.00%), Space: 7.5 MB (61.09%) - LeetHub

---
+++ 3021-alice-and-bob-playing-flower-game/3021-alice-and-bob-playing-flower-game.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+    long long flowerGame(int n, int m) {
+        
+//         long long ans=0;
+//         int oddcount1=0;
+//         int evencount1=0;
+        
+//          int oddcount2=0;
+//         int evencount2=0;
+        
+        
+//         if(n%2)
+//         {
+//             oddcount1=(n+1)/2;
+//             evencount1=n-oddcount1;
+//         }else
+//         {
+//             oddcount1=(n)/2;
+//             evencount1=n-oddcount1;
+// }
+        
+         
+//         if(m%2)
+//         {
+//             oddcount2=(m+1)/2;
+//             evencount2=m-oddcount2;
+//         }else
+//         {
+//    oddcount2=(m)/2;
+//             evencount2=m-oddcount2;}
+        
+        
+     
+        
+//         ans=(oddcount1*1ll)*evencount2*1ll+(evencount1*1ll)*(oddcount2*1ll);
+        
+
+        
+        
+        
+        
+        
+        
+        
+        // return ans;
+      
+                return 1ll*n*1ll*m/2;
+        
+        
+        
+        
+        
+        
+        
+    }
+};
