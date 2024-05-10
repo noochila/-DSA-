41ac8cfe9bcc92e493a7f6b28f29ef2281b48fed
Manoj N <112234009+Noochila@users.noreply.github.com>
2024-05-11 04:03:52 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Juggler Sequence/juggler-sequence.cpp
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    vector<int> jugglerSequence(int n) {
+        // code here
+        
+        
+      vector<int> ans;
+       
+      while(n!=1){
+          ans.push_back(n);
+          if(n%2){
+            n=n*(sqrt(n));
+          }
+          else{
+             n=sqrt(n);
+          }
+      }
+      ans.push_back(n);
+      
+      return ans;
+        
+        
+        
+        
+        
+        
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        Solution ob;
+        vector<int> ans = ob.jugglerSequence(n);
+        for (int u : ans)
+            cout << u << " ";
+        cout << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
