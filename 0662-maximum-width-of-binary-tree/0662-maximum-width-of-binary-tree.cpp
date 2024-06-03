ecb75f562b4f6e4dc2a7faa7cf6742873d22ee7e
Manoj N <112234009+Noochila@users.noreply.github.com>
2024-06-03 10:47:22 +0530

    Time: 13 ms (8.09%), Space: 18.1 MB (78.31%) - LeetHub

--- 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
+++ 0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.cpp
@@ -1,46 +1,46 @@
-						// \U0001f609\U0001f609\U0001f609\U0001f609Please upvote if it helps \U0001f609\U0001f609\U0001f609\U0001f609
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+#include <queue>
+#include <algorithm>
+#define ll long long
+
 class Solution {
 public:
     int widthOfBinaryTree(TreeNode* root) {
-        if(root == NULL)
-            return 0;
-        
-        long long res = 1;
-        queue<pair<TreeNode*, long long>> q;
-        
-        // I am using intialising list
-        q.push({root, 0});      // also can use make_pair
-        
-        while(!q.empty())
-        {
-            long long cnt = q.size();
-            // start is the index of root node for first level
-            long long start = q.front().second;
-            long long end = q.back().second;
-            
-            res = max(res,end-start + 1);
-            
-            for(int i = 0; i <cnt; ++i)
-            {
-                pair<TreeNode*, int> p = q.front();
-                // we will use it while inserting it children
-                // left child will be 2 * idx + 1;
-                // right chils will be 2 * idx + 2;
-                long long idx = p.second - start;
-                
+        if (!root) return 0;
+
+        ll maxi = 0;
+        std::queue<std::pair<TreeNode*, ll>> q;
+        q.push({root, 0});
+
+        while (!q.empty()) {
+            ll size = q.size();
+            ll minIndex = q.front().second;  // Use the index of the first node to normalize indices
+            ll first, last;
+            for (ll i = 0; i < size; ++i) {
+                ll curIndex = q.front().second - minIndex;  // Normalize to prevent overflow
+                TreeNode* node = q.front().first;
                 q.pop();
-                
-                // if  left child exist
-                if(p.first->left != NULL)
-                    q.push({p.first->left, (long long)2 * idx + 1});
-                
-                // if right child exist
-                if(p.first->right != NULL)
-                    q.push({p.first->right, (long long) 2 * idx + 2});
+
+                if (i == 0) first = curIndex;
+                if (i == size - 1) last = curIndex;
+
+                if (node->left) q.push({node->left, 2 * curIndex + 1});
+                if (node->right) q.push({node->right, 2 * curIndex + 2});
             }
+            maxi = std::max(maxi, last - first + 1);
         }
-        
-        return res;
-        
+
+        return maxi;
     }
 };
