b37e369e89c546a52f890c5fe9dda7e69d362b54
Manoj N <112234009+Noochila@users.noreply.github.com>
2024-06-07 12:34:00 +0530

    Time: 33 ms (40.89%), Space: 37 MB (36.24%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
@@ -11,56 +11,72 @@
  * };
  */
 
-class BSTIteratorleft {
+class BSTIterator
+{
 public:
-    stack<TreeNode*> s;
-    BSTIteratorleft(TreeNode* root) { pushALL(root); }
+    stack<TreeNode *> s;
+    bool reverse; // reverse is true means it's before else it's next
 
-    int next() {
-        TreeNode* temp = s.top();
-        s.pop();
-        pushALL(temp->right);
+    BSTIterator(TreeNode *root, bool reverse)
+    {
+        this->reverse = reverse;
+        pushALL(root);
+    }
+
+    int next()
+    {
+        TreeNode *temp = s.top();
+        if (!reverse)
+        {
+
+            s.pop();
+            pushALL(temp->right);
+        }
+        else
+        {
+
+            s.pop();
+            pushALL(temp->left);
+        }
+
         return temp->val;
     }
 
-    bool hasNext() { return !s.empty(); }
+    bool hasNext()
+    {
 
-    void pushALL(TreeNode* root) {
-        while (root) {
-            s.push(root);
-            root = root->left;
+        return !s.empty();
+    }
+
+    void pushALL(TreeNode *root)
+    {
+        if (!reverse)
+        {
+            while (root)
+            {
+                s.push(root);
+                root = root->left;
+            }
+        }
+        else
+        {
+            while (root)
+            {
+                s.push(root);
+                root = root->right;
+            }
         }
     }
 };
-class BSTIteratorright {
-public:
-    stack<TreeNode*> s;
-    BSTIteratorright(TreeNode* root) { pushALL(root); }
 
-    int next() {
-        TreeNode* temp = s.top();
-        s.pop();
-        pushALL(temp->left);
-        return temp->val;
-    }
-
-    bool hasNext() { return !s.empty(); }
-
-    void pushALL(TreeNode* root) {
-        while (root) {
-            s.push(root);
-            root = root->right;
-        }
-    }
-};
 class Solution {
 public:
     bool findTarget(TreeNode* root, int k) {
 
         if(root==nullptr)
         return false;
-    BSTIteratorleft l(root);
-    BSTIteratorright r(root);
+    BSTIterator l(root,false);
+    BSTIterator r(root,true);
     int i=l.next(),j=r.next();
 
     while(i<j)
