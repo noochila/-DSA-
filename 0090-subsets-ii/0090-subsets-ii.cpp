203bd41ed7867fdf6724256ba05e8db7eb49d1cf
Manoj N <112234009+Noochila@users.noreply.github.com>
2024-06-12 03:06:46 +0530

    Time: 3 ms (57.87%), Space: 15.9 MB (10.96%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+
+class Solution {
+    set<vector<int>> s;
+
+    private:
+void printarr(vector<int> &v, int n, vector<int> x, int index) {
+  if (index >= n) {
+    sort(x.begin(),x.end());
+    s.insert(x);
+    return;
+  }
+  printarr(v, n, x, index + 1);
+  x.push_back(v[index]);
+  printarr(v, n, x, index + 1);
+
+}
+
+public:
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+              vector<int> x;
+  printarr(nums, nums.size(), x, 0);
+
+  vector<vector<int>> ans(s.begin(),s.end());
+  
+  return ans;
+        
+    }
+};
