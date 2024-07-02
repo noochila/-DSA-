48c2609ca65cef1162469d6749d69e60a4a87f34
Manoj N <112234009+Noochila@users.noreply.github.com>
2024-07-02 10:48:47 +0530

    Time: 83 ms (8.16%), Space: 33.7 MB (5.19%) - LeetHub

--- 0876-hand-of-straights/0876-hand-of-straights.cpp
+++ 0876-hand-of-straights/0876-hand-of-straights.cpp
@@ -1,69 +1,85 @@
-// #include <iostream>
-// #include <vector>
-// #include <algorithm>
+#include <iostream>
+#include <vector>
+#include <algorithm>
 
-// class Solution
-// {
-// public:
-//     bool isNStraightHand(vector<int>& hand, int k) {
-//     {
-//         // sort(hand.begin(), hand.end());
-
-//         // while (!hand.empty()) {
-//         //     std::vector<int> demo;
-//         //     demo.push_back(hand.front());
-//         //     hand.erase(hand.begin());
-
-//         //     for (int j = 0; j < groupSize - 1; ++j) {
-//         //         auto it = std::find(hand.begin(), hand.end(), demo.back() + 1);
-//         //         if (it != hand.end()) {
-//         //             demo.push_back(*it);
-//         //             hand.erase(it);
-//         //         } else {
-//         //             return false; // Sequence cannot be formed
-//         //         }
-//         //     }
-
-//         // }
-
-//         // return true;
-
-        
-//     }
-// };
-
-
-class Solution {
+class Solution
+{
 public:
-    bool isNStraightHand(vector<int>& hand, int k) {
-      if(hand.size()%k!=0)
-       return false;
-       priority_queue<int,vector<int>,greater<int>>pq(hand.begin(),hand.end());
-       while(!pq.empty()){
-           vector<int>remain;
-           int cycle=k;
-           while(cycle>1&&!pq.empty()){
-               int temp=pq.top();
-               pq.pop();
-               while(pq.top()==temp&&!pq.empty()){
-               remain.push_back(pq.top());
-               pq.pop();
-               }
-               if(pq.top()-temp==1&&!pq.empty())
-              cycle--;
-               else
-               return false;
-           }
-              pq.pop();
-              for(auto it=remain.begin();it!=remain.end();it++){
-               pq.push(*it);
-                   }
-                   if(pq.empty())
-                   return true;
-           }
-        
-       
-       return true;
-       
+    bool isNStraightHand(std::vector<int> &arr, int g)
+    {
+        // sort(hand.begin(), hand.end());
+
+        // while (!hand.empty()) {
+        //     std::vector<int> demo;
+        //     demo.push_back(hand.front());
+        //     hand.erase(hand.begin());
+
+        //     for (int j = 0; j < groupSize - 1; ++j) {
+        //         auto it = std::find(hand.begin(), hand.end(), demo.back() + 1);
+        //         if (it != hand.end()) {
+        //             demo.push_back(*it);
+        //             hand.erase(it);
+        //         } else {
+        //             return false; // Sequence cannot be formed
+        //         }
+        //     }
+
+        // }
+
+        // return true;
+
+        if (arr.size() % g != 0)
+            return false;
+
+        unordered_map<int, int> mp;
+        for (int i = 0; i < arr.size(); i++)
+            mp[arr[i]]++;
+
+        priority_queue<int, vector<int>, greater<int>> pq;
+
+        for (auto it : mp)
+            pq.push(it.first);
+
+        while (!pq.empty())
+        {
+
+            vector<int> demo;
+            cout << pq.top() << endl;
+            demo.push_back(pq.top());
+            mp[pq.top()]--;
+            pq.pop();
+
+            if (pq.empty() && g!=1)
+                return false;
+
+            for (int i = 0; i < g - 1 && !pq.empty(); i++)
+            {
+
+                if (demo.back() + 1 == pq.top())
+                {
+                    if (mp[pq.top()] != 0)
+                    {
+                        cout << pq.top() << endl;
+                        mp[pq.top()]--;
+                        demo.push_back(pq.top());
+                        pq.pop();
+                    }
+                    else
+                        return false;
+                }
+                else
+
+                {
+                    return false;
+                }
+            }
+
+            for (auto it : demo)
+            {
+                if (mp[it] > 0)
+                    pq.push(it);
+            }
+        }
+        return true;
     }
 };
