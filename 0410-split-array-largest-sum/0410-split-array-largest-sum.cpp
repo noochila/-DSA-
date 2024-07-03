9ac16f9826270da05f42c03c141aee5eacf294b0
Manoj N <112234009+Noochila@users.noreply.github.com>
2024-07-03 14:47:56 +0530

    Time: 0 ms (100%), Space: 9.2 MB (33.73%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -1,46 +1,42 @@
 class Solution {
 public:
    
-    bool check(int mid,vector<int>&nums,int k)
-    {
-        int s=1;
-        int ct=0;
-        for(auto it:nums)
-        {
-            if(ct+it>mid)
-            {
-                s++;
-                ct=it;
-            }else
-            ct+=it;
-        }
 
-        if(s>k)
-        return true;
-        else
-        return false;
+int countPartitions(vector<int> &a, int maxSum) {
+    int n = a.size(); //size of array.
+    int partitions = 1;
+    long long subarraySum = 0;
+    for (int i = 0; i < n; i++) {
+        if (subarraySum + a[i] <= maxSum) {
+            //insert element to current subarray
+            subarraySum += a[i];
+        }
+        else {
+            //insert element to next subarray
+            partitions++;
+            subarraySum = a[i];
+        }
     }
+    return partitions;
+}
 
 
-    int splitArray(vector<int>& nums, int k) {
+    int splitArray(vector<int>& a, int k) {
 
-        if(nums.size()<k)
-        return 0;
-
-
-        int low=*max_element(nums.begin(),nums.end());
-        int high=accumulate(nums.begin(),nums.end(),0);
-
-        while(low<=high)
-        {
-            int mid=(low+high)/2;
-
-            if(check(mid,nums,k))
-            low=mid+1;
-            else
-            high=mid-1;
+    int low = *max_element(a.begin(), a.end());
+    int high = accumulate(a.begin(), a.end(), 0);
+    //Apply binary search:
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        int partitions = countPartitions(a, mid);
+        if (partitions > k) {
+            low = mid + 1;
         }
-        return high+1;
+        else {
+            high = mid - 1;
+        }
+    }
+    return low;
         
     }
 };
