2f6f22d2a6f0c2658e8159060445ba55faf3441b
Manoj N <112234009+Noochila@users.noreply.github.com>
2024-07-07 18:37:26 +0530

    Updated Solution - GfG to GitHub

---
+++ Difficulty: Hard/Aggressive Cows/aggressive-cows.cpp
@@ -0,0 +1,91 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+public:
+
+
+ bool check(vector<int>&stalls,int k,int dis)
+ {
+     
+     int start=0;
+     int c=1;
+     
+     for(int i=1;i<stalls.size();i++)
+     {
+         if(stalls[i]-stalls[start]>=dis)
+         {
+             c++;
+             start=i;
+         }
+         
+         if(c>=k)
+         return true;
+     }
+     
+     return false;
+     
+ }
+
+    int solve(int n, int k, vector<int> &stalls) {
+    
+        // Write your code here
+        
+        sort(stalls.begin(),stalls.end());
+        
+        int low=0;
+        int high=*max_element(stalls.begin(),stalls.end())-*min_element(stalls.begin(),stalls.end());
+        
+        
+        while(low<=high)
+        {
+            int mid=(low+high)/2;
+            
+            if(check(stalls,k,mid))
+            {
+                low=mid+1;
+            }else
+               high=mid-1;
+        }
+        
+        return high;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t = 1;
+    cin >> t;
+
+    // freopen ("output_gfg.txt", "w", stdout);
+
+    while (t--) {
+        // Input
+
+        int n, k;
+        cin >> n >> k;
+
+        vector<int> stalls(n);
+        for (int i = 0; i < n; i++) {
+            cin >> stalls[i];
+        }
+        // char ch;
+        // cin >> ch;
+
+        Solution obj;
+        cout << obj.solve(n, k, stalls) << endl;
+
+        // cout << "~\n";
+    }
+    // fclose(stdout);
+
+    return 0;
+}
+// } Driver Code Ends
