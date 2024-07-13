182593b8480721abde7530ee5cce862560d17e67
Manoj N <112234009+Noochila@users.noreply.github.com>
2024-07-13 16:42:47 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Disjoint set (Union-Find)/disjoint-set-union-find.cpp
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+int find(int a[],int x);
+void unionSet(int a[],int x ,int z);
+int main(){
+	int t;
+	cin>>t;
+	while(t--){
+		int n;
+		cin>>n;
+		int a[n+1];
+		for(int i=1;i<=n;i++)
+			a[i]=i;
+		int k;
+		cin>>k;
+		for(int i=0;i<k;i++){
+			string s;
+			cin>>s;
+			if(s=="UNION"){
+				int z,x;
+				cin>>x>>z;
+				unionSet(a,x,z);
+			}
+			else{
+				int x;
+				cin>>x;
+				int parent=find(a,x);
+				cout<<parent<<" ";
+			}
+		}
+		cout<<endl;
+	}
+}
+// } Driver Code Ends
+
+
+/*Complete the functions below*/
+int find(int A[],int X)
+{
+       //add code here
+       
+      if(A[X]==X)
+      return X;
+      else 
+      return A[X]=find(A,A[X]);
+      
+}
+void unionSet(int A[],int X,int Z)
+{
+	//add code here.
+	
+	
+	int up_x=find(A,X);
+	int up_y=find(A,Z);
+	
+	A[up_x]=up_y;
+	
+	
+	
+}
