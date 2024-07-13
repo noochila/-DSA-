7f1bb4d84576b460926a203da307edbd99fd2cb4
Manoj N <112234009+Noochila@users.noreply.github.com>
2024-07-13 18:31:21 +0530

    Time: 7 ms (95.26%), Space: 12.7 MB (52.43%) - LeetHub

--- 0794-swim-in-rising-water/0794-swim-in-rising-water.cpp
+++ 0794-swim-in-rising-water/0794-swim-in-rising-water.cpp
@@ -1,21 +1,26 @@
+
 class Solution {
 public:
     int swimInWater(vector<vector<int>>& grid) {
+
         int n = grid.size();
-        vector<vector<int>> vis(n, vector<int>(n, 0));
+        vector<vector<int>> vis(n + 1, vector<int>(n + 1, 0));
        
-        priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>> pq;
+        priority_queue<pair<int, pair<int, int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>> pq;
         pq.push({grid[0][0], {0, 0}});
         vis[0][0] = 1;
 
         while (!pq.empty()) {
-            auto temp = pq.top();
-            pq.pop();
-            int time = temp.first;
-            int i = temp.second.first;
-            int j = temp.second.second;
 
-            if (i == n - 1 && j == n - 1) return time;
+            auto temp=pq.top();
+            pq.pop();
+            int time=temp.first;
+            int i=temp.second.first;
+            int j=temp.second.second;
+
+            if(i==n-1 && j==n-1)
+            return time;
+
 
             int dx[] = {-1, 0, 1, 0};
             int dy[] = {0, 1, 0, -1};
@@ -24,9 +29,15 @@ public:
                 int x = i + dx[k];
                 int y = j + dy[k];
 
-                if (x >= 0 && x < n && y >= 0 && y < n && !vis[x][y]) {
-                    vis[x][y] = 1;
-                    pq.push({max(time, grid[x][y]), {x, y}});
+                if (x >= 0 && x < n && y >= 0 && y < n) {
+
+                    if (grid[x][y] <= time && vis[x][y] == 0) {
+                        pq.push({time,{x,y}});
+                        vis[x][y]=1;
+                    } else if (grid[x][y] > time && vis[x][y] == 0) {
+                        pq.push({grid[x][y],{x,y}});
+                         vis[x][y]=1;
+                    }
                 }
             }
         }
