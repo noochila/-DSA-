91df73fe0c5eaf6b6d7e20820d3685c59a5d9dcb
Manoj N <112234009+Noochila@users.noreply.github.com>
2024-07-21 01:23:37 +0530

    Time: 36 ms (67.04%), Space: 36.1 MB (12.19%) - LeetHub

--- 1711-find-valid-matrix-given-row-and-column-sums/1711-find-valid-matrix-given-row-and-column-sums.cpp
+++ 1711-find-valid-matrix-given-row-and-column-sums/1711-find-valid-matrix-given-row-and-column-sums.cpp
@@ -1,35 +1,25 @@
 class Solution {
 public:
     vector<vector<int>> restoreMatrix(vector<int>& rowSum, vector<int>& colSum) {
-        int rows = rowSum.size();
-        int cols = colSum.size();
-        int cur_row = 0, cur_col = 0;
-        vector<vector<int>> res(rows, vector<int>(cols, 0));
+        int n=rowSum.size();
+        int m=colSum.size();
 
-        while (cur_row < rows || cur_col < cols) {
-            if (cur_row >= rows) {
-                res[rows - 1][cur_col] = colSum[cur_col];
-                cur_col++;
-                continue;
-            } else if (cur_col >= cols) {
-                res[cur_row][cols - 1] = rowSum[cur_row];
-                cur_row++;
-                continue;
-            }
+        int i=0;
+        int j=0;
 
-            int value_to_put = min(rowSum[cur_row], colSum[cur_col]);
-            rowSum[cur_row] -= value_to_put;
-            colSum[cur_col] -= value_to_put;
-            res[cur_row][cur_col] = value_to_put;
+        vector<vector<int>> vis(n,vector<int>(m,0));
 
-            // I write this as this because it's possible that rowSum[cur_row] == colSum[cur_col] and we'll want to move both row and col pointers
-            if (rowSum[cur_row] == 0) {
-                cur_row++;
-            }
-            if (colSum[cur_col] == 0) {
-                cur_col++;
-            }
+        while(i<n && j<m)
+        {
+            vis[i][j]=min(rowSum[i],colSum[j]);
+            rowSum[i]-=vis[i][j];
+            colSum[j]-=vis[i][j];
+            if(rowSum[i]==0)
+            i++;
+            else
+            j++;
         }
-        return res;
+        return vis;
+        
     }
 };
