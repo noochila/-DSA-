b38095c9975b2940208bd74f3a9c12969f9a6826
Manoj N <112234009+Noochila@users.noreply.github.com>
2024-07-22 21:10:36 +0530

    Updated Solution - GfG to GitHub

---
+++ Difficulty: Medium/Largest BST/largest-bst.cpp
@@ -0,0 +1,179 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+
+#include <bits/stdc++.h>
+using namespace std;
+#define MAX_HEIGHT 100000
+
+// Tree Node
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+};
+
+// Utility function to create a new Tree Node
+Node* newNode(int val) {
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+
+    return temp;
+}
+
+
+// Function to Build Tree
+Node* buildTree(string str) {
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N') return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str;) ip.push_back(str);
+
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N") {
+
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size()) break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N") {
+
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+
+// } Driver Code Ends
+/* Tree node structure  used in the program
+
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};*/
+
+class Solution{
+    public:
+    /*You are required to complete this method */
+    // Return the size of the largest sub-tree which is also a BST
+    
+    
+    pair<int,pair<int,int>> solve(Node* root)
+    {
+        if(root==nullptr)
+        {
+            return {0,{INT_MIN,INT_MAX}};
+        }
+        else if(root->left==nullptr && root->right==nullptr)
+        {
+            return {1,{root->data,root->data}};
+        }
+        
+        
+        auto left=solve(root->left);
+        auto right=solve(root->right);
+        
+        // cout<<left.first<<" "<<left.second.first<<" "<<left.second.second<<" - "<<right.first<<" "<<right.second.first<<" "<<right.second.second<<endl;
+        
+        if(left.second.first<root->data && root->data<right.second.second)
+        {
+            return {left.first+right.first+1,{max(right.second.first,root->data),min(left.second.second,root->data)}};
+        }else
+        {
+            return {max(left.first,right.first),{INT_MAX,INT_MIN}};
+        }
+        
+        
+    }
+    
+   
+    int largestBst(Node *root)
+    {
+    	//Your code here
+    	
+    	
+    	
+    	auto ans=solve(root);
+    	
+    	   //cout<<ans.first<<" "<<ans.second.first<<" "<<ans.second.second<<endl;
+    	
+    	
+    	
+    	return ans.first;
+    	
+    	
+    	
+    }
+};
+
+
+//{ Driver Code Starts.
+
+/* Driver program to test size function*/
+
+  
+
+int main() {
+
+   
+    int t;
+    scanf("%d ", &t);
+    while (t--) {
+        string s, ch;
+        getline(cin, s);
+        
+        Node* root = buildTree(s);
+        Solution ob;
+        cout << ob.largestBst(root) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
