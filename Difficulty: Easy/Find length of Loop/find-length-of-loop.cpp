081028a143330da6be3c08cdee1c6fb67a3f937a
Manoj N <112234009+Noochila@users.noreply.github.com>
2024-07-26 00:04:11 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Find length of Loop/find-length-of-loop.cpp
@@ -0,0 +1,130 @@
+//{ Driver Code Starts
+// driver code
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node* next;
+    
+    Node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+void loopHere(Node* head, Node* tail, int position)
+{
+    if(position==0) return;
+    
+    Node* walk = head;
+    for(int i=1; i<position; i++)
+        walk = walk->next;
+    tail->next = walk;
+}
+
+int countNodesinLoop(Node* head);
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n, num;
+        cin>>n;
+        
+        Node *head, *tail;
+        cin>> num;
+        head = tail = new Node(num);
+        
+        for(int i=0 ; i<n-1 ; i++)
+        {
+            cin>> num;
+            tail->next = new Node(num);
+            tail = tail->next;
+        }
+        
+        int pos;
+        cin>> pos;
+        loopHere(head,tail,pos);
+        
+        cout<< countNodesinLoop(head) << endl;
+    }
+	return 0;
+}
+
+// } Driver Code Ends
+
+
+/*
+
+struct Node {
+    int data;
+    struct Node *next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+*/
+
+//Function to find the length of a loop in the linked list.
+
+int count(struct Node* fast,struct Node* slow)
+{ int cnt = 1;
+    
+    // move fast by one step
+    fast = fast->next;
+    
+    // traverse fast till it 
+    // reaches back to slow
+    while(slow!=fast){
+        
+        // at each node increase
+        // count by 1 and move fast
+        // forward by one step
+        cnt++;
+        fast = fast->next;
+    }
+    
+    // loop terminates when fast reaches
+    // slow again and the count is returned
+    return cnt;
+}
+int countNodesinLoop(struct Node *head)
+{
+    // Code here
+    
+    
+    
+    struct Node *fast=head;
+    struct Node *slow=head;
+    
+     while (fast != nullptr && fast->next != nullptr) {
+        // Move slow one step
+        slow = slow->next;     
+        // Move fast two steps
+        fast = fast->next->next;
+
+        // Step 2: If the slow and fast pointers
+        // meet, there is a loop
+        if (slow == fast) {
+            // return the number of nodes
+            // in the loop
+            return count(slow, fast);
+        }
+    }
+
+    // Step 3: If the fast pointer
+    // reaches the end, there is no loop
+    return 0; 
+    
+    
+    
+    
+}
