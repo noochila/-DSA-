37e1bf1a62d2124b3e0fba74260ebe54efe66973
Manoj N <112234009+Noochila@users.noreply.github.com>
2024-07-28 15:14:43 +0530

    Time: 86 ms (24.68%), Space: 74.5 MB (17.35%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -1,32 +1,36 @@
 class Solution {
 public:
- int longestCommonSubsequence(string s1, string s2) {
+  int longestCommonSubsequence(string text1, string text2) {
+        
+       int n=text1.size();
+       int m=text2.size();
+        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
 
-        int n = s1.size();
-        int m = s2.size();
-        // return solve(text1,text2,n,m);
+        // for(int i=0;i<=n;i++)
+        // dp[i][0]=0;
 
-       vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1)); // Create a DP table
+        // for(int j=0;j<=m;j++)
+        // dp[0][j]=0;
 
-    // Initialize the base cases
-    for (int i = 0; i <= n; i++) {
-        dp[i][0] = 0;
-    }
-    for (int i = 0; i <= m; i++) {
-        dp[0][i] = 0;
-    }
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=m;j++)
+            {
+                if(text1[i-1]==text2[j-1])
+                {
+                    dp[i][j]=1+dp[i-1][j-1];
+                }else
+                {
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                }
 
-    // Fill in the DP table to calculate the length of LCS
-    for (int ind1 = 1; ind1 <= n; ind1++) {
-        for (int ind2 = 1; ind2 <= m; ind2++) {
-            if (s1[ind1 - 1] == s2[ind2 - 1])
-                dp[ind1][ind2] = 1 + dp[ind1 - 1][ind2 - 1]; // Characters match, increment LCS length
-            else
-                dp[ind1][ind2] = max(dp[ind1 - 1][ind2], dp[ind1][ind2 - 1]); // Characters don't match, consider the maximum from left or above
+               // cout<<dp[i][j]<<" ";
+            }
+            //cout<<endl;
         }
-    }
 
-    return dp[n][m]; // Return the length of the Longest Common Subsequence
+        return dp[n][m];
+
     }
     int longestPalindromeSubseq(string s) {
         string t=s;
