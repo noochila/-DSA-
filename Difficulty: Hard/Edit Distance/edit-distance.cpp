953476b0dbdc77fd99de6ea6e2cee6f2faab78c2
Manoj N <112234009+Noochila@users.noreply.github.com>
2024-08-02 22:34:01 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Edit Distance/edit-distance.cpp
@@ -0,0 +1,68 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+  
+  
+  int editDistanceUtil(string& S1, string& S2, int i, int j, vector<vector<int>>& dp) {
+    // Base cases
+    if (i < 0)
+        return j + 1;
+    if (j < 0)
+        return i + 1;
+
+    // If the result for this state has already been calculated, return it
+    if (dp[i][j] != -1)
+        return dp[i][j];
+
+    // If the characters at the current positions match, no operation is needed
+    if (S1[i] == S2[j])
+        return dp[i][j] = 0 + editDistanceUtil(S1, S2, i - 1, j - 1, dp);
+
+    // Minimum of three choices:
+    // 1. Replace the character at S1[i] with the character at S2[j]
+    // 2. Delete the character at S1[i]
+    // 3. Insert the character at S2[j] into S1
+    else
+        return dp[i][j] = 1 + min(editDistanceUtil(S1, S2, i - 1, j - 1, dp),
+                                  min(editDistanceUtil(S1, S2, i - 1, j, dp),
+                                      editDistanceUtil(S1, S2, i, j - 1, dp)));
+}
+
+
+  
+    int editDistance(string S1, string S2) {
+        // Code here
+        int n = S1.size();
+    int m = S2.size();
+
+    // Create a DP table to memoize results
+    vector<vector<int>> dp(n, vector<int>(m, -1));
+
+    // Call the utility function with the last indices of both strings
+    return editDistanceUtil(S1, S2, n - 1, m - 1, dp);
+        
+        
+        
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        string s, t;
+        cin >> s >> t;
+        Solution ob;
+        int ans = ob.editDistance(s, t);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
