301e7d1d1197807b0fa3f122ff6f2449c8d026c3
Manoj N <112234009+Noochila@users.noreply.github.com>
2024-08-09 18:42:44 +0530

    Time: 1273 ms (5%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -3,5 +3,5 @@ FROM weather AS w1
 WHERE w1.temperature > (
     SELECT w2.temperature 
     FROM weather AS w2 
-    WHERE DATE(w2.recordDate) = DATE_SUB(DATE(w1.recordDate), INTERVAL 1 DAY)
+    WHERE DATEDIFF(w1.recordDate, w2.recordDate) = 1
 );
