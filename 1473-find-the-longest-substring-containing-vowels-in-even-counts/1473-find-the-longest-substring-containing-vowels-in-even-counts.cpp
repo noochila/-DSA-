7f20604c928b7c96238e57fbf9b9bb2ff489d2de
Manoj N <112234009+Noochila@users.noreply.github.com>
2024-09-15 15:01:22 +0530

    Time: 70 ms (58.37%), Space: 18.9 MB (32.79%) - LeetHub

--- 1473-find-the-longest-substring-containing-vowels-in-even-counts/1473-find-the-longest-substring-containing-vowels-in-even-counts.cpp
+++ 1473-find-the-longest-substring-containing-vowels-in-even-counts/1473-find-the-longest-substring-containing-vowels-in-even-counts.cpp
@@ -1,46 +1,74 @@
 class Solution {
 public:
     int findTheLongestSubstring(string s) {
+        // int ans = 0;
+
+        // unordered_map<string, int> mp;
+        // mp["00000"] = -1;
+
+        // string temp = "00000";
+
+        // for (int i = 0; i < s.size(); i++) {
+
+        //     if (s[i] == 'a') {
+        //         if (temp[0] == '0')
+        //             temp[0] = '1';
+        //         else
+        //             temp[0] = '0';
+        //     } else if (s[i] == 'e') {
+        //         if (temp[1] == '0')
+        //             temp[1] = '1';
+        //         else
+        //             temp[1] = '0';
+        //     } else if (s[i] == 'i') {
+        //         if (temp[2] == '0')
+        //             temp[2] = '1';
+        //         else
+        //             temp[2] = '0';
+        //     } else if (s[i] == 'o') {
+        //         if (temp[3] == '0')
+        //             temp[3] = '1';
+        //         else
+        //             temp[3] = '0';
+        //     } else if (s[i] == 'u') {
+        //         if (temp[4] == '0')
+        //             temp[4] = '1';
+        //         else
+        //             temp[4] = '0';
+        //     }
+
+        //     if (mp.find(temp) == mp.end())
+        //         mp[temp] = i;
+        //     else
+        //         ans = max(ans, i - mp[temp]);
+        // }
+
+        // return ans;
         int ans = 0;
 
-        unordered_map<string, int> mp;
-        mp["00000"] = -1;
+        unordered_map<int, int> mp;
+        mp[0] = -1;
 
-        string temp = "00000";
-
+        int mask = 0;
         for (int i = 0; i < s.size(); i++) {
 
             if (s[i] == 'a') {
-                if (temp[0] == '0')
-                    temp[0] = '1';
-                else
-                    temp[0] = '0';
+                mask = mask ^ (1 << 0);
+
             } else if (s[i] == 'e') {
-                if (temp[1] == '0')
-                    temp[1] = '1';
-                else
-                    temp[1] = '0';
+                mask = mask ^ (1 << 1);
             } else if (s[i] == 'i') {
-                if (temp[2] == '0')
-                    temp[2] = '1';
-                else
-                    temp[2] = '0';
+                mask = mask ^ (1 << 2);
             } else if (s[i] == 'o') {
-                if (temp[3] == '0')
-                    temp[3] = '1';
-                else
-                    temp[3] = '0';
+                mask = mask ^ (1 << 3);
             } else if (s[i] == 'u') {
-                if (temp[4] == '0')
-                    temp[4] = '1';
-                else
-                    temp[4] = '0';
+                mask = mask ^ (1 << 4);
             }
 
-            if (mp.find(temp) == mp.end())
-                mp[temp] = i;
+            if (mp.find(mask) == mp.end())
+                mp[mask] = i;
             else
-                ans = max(ans, i - mp[temp]);
+                ans = max(ans, i - mp[mask]);
         }
 
         return ans;
