f6859127a6279bad9352e900fe3a58835d8a0969
Manoj N <112234009+Noochila@users.noreply.github.com>
2024-09-28 01:37:55 +0530

    Time: 141 ms (72.68%), Space: 42.8 MB (11.91%) - LeetHub

--- 0731-my-calendar-ii/0731-my-calendar-ii.cpp
+++ 0731-my-calendar-ii/0731-my-calendar-ii.cpp
@@ -1,31 +1,43 @@
 class MyCalendarTwo {
+
 public:
-    vector<pair<int, int>> s;
-    vector<pair<int, int>> d;
 
-    MyCalendarTwo() {}
-
-    bool checkOverlap(int a1, int a2, int b1, int b2) {
-        if (max(a1, a2) < min(b1, b2))
-            return true;
-        else
-            return false;
+    map<int,int>mp;
+    MyCalendarTwo() {
+        
     }
-
+    
     bool book(int start, int end) {
 
-        for (auto it : d) {
-            if (checkOverlap(it.first, start, it.second, end))
-                return false;
-        }
+        mp[start]++;
+        mp[end]--;
 
-        for (auto it : s) {
-            if (checkOverlap(it.first, start, it.second, end)) {
-                d.push_back({max(it.first, start), min(it.second, end)});
+        int c=0;
+        for(auto it:mp)
+        {
+            c+=it.second;
+            if(c>2)
+            {
+                mp[start]--;
+                mp[end]++;
+            
+
+            if(mp[start]==0)
+            mp.erase(start);
+
+            if(mp[end]==0)
+            mp.erase(end);
+            return false;
             }
-        }
 
-        s.push_back({start, end});
+        }
         return true;
+        
     }
 };
+
+/**
+ * Your MyCalendarTwo object will be instantiated and called as such:
+ * MyCalendarTwo* obj = new MyCalendarTwo();
+ * bool param_1 = obj->book(start,end);
+ */
