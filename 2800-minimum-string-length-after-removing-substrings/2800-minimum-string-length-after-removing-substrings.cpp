95efc96e93677cf523c3b81b61b08a6c7f67b6b2
Manoj N <112234009+Noochila@users.noreply.github.com>
2024-10-07 12:41:44 +0530

    Time: 3 ms (89.38%), Space: 11.6 MB (61.24%) - LeetHub

--- 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.cpp
+++ 2800-minimum-string-length-after-removing-substrings/2800-minimum-string-length-after-removing-substrings.cpp
@@ -2,21 +2,49 @@ class Solution {
 public:
     int minLength(string s) {
 
-        while (true) {
-            if (s.find("AB") != string::npos) {
-                s.erase(s.find("AB"), 2);
-            }
+        // while (true) {
+        //     if (s.find("AB") != string::npos) {
+        //         s.erase(s.find("AB"), 2);
+        //     }
 
-            if (s.find("CD") != string::npos) {
-                s.erase(s.find("CD"), 2);
-            }
+        //     if (s.find("CD") != string::npos) {
+        //         s.erase(s.find("CD"), 2);
+        //     }
 
         
 
-            if (s.find("AB") == string::npos && s.find("CD") == string::npos )
-                break;
+        //     if (s.find("AB") == string::npos && s.find("CD") == string::npos )
+        //         break;
+        // }
+
+        // return s.size();
+
+
+        stack<char> st;
+
+        for(int i=0;i<s.size();i++)
+        {
+             char ch=s[i];
+
+             if(st.empty())
+             {
+                st.push(ch);
+                continue;
+             }
+
+             if(!st.empty()  &&  st.top()=='A' && s[i]=='B')
+             {
+                st.pop();
+             }
+
+            else if(!st.empty() && st.top()=='C' && s[i]=='D')
+             st.pop();
+             else
+             st.push(ch);
+
+
         }
 
-        return s.size();
+        return st.size();
     }
 };
