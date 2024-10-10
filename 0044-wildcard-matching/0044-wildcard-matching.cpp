e44c017d62345c274daabb91b28a9a55c2170dcf
Manoj N <112234009+Noochila@users.noreply.github.com>
2024-10-11 00:29:52 +0530

    Time: 42 ms (49.73%), Space: 32 MB (11.21%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -1,24 +1,37 @@
 class Solution {
 public:
-    bool isMatch(string s, string p) {
-        int m = s.length(), n = p.length();
-        int i = 0, j = 0, asterick = -1, match;
-        while (i < m) {
-            if (j < n && p[j] == '*') {
-                match = i;  
-                asterick = j++;
+    int f(int i,int j, string &s,string &p,vector<vector<int>> &dp)
+    {
+        if(dp[i][j]!=-1) return dp[i][j];
+        if(i==0 && j==0) return dp[i][j]=1;
+        if(j==0 && i>0) return dp[i][j]=0;
+        if(i==0 && j>0)
+        {
+            while(j>0)
+            {
+                if(p[j-1]=='*') j--;
+                else return dp[i][j]=0;
             }
-            else if (j < n && (s[i] == p[j] || p[j] == '?')) {
-                i++;
-                j++;
-            }
-            else if (asterick >= 0) {
-                i = ++match;
-                j = asterick + 1;
-            }
-            else return false;
+            return dp[i][j]=1;
         }
-        while (j < n && p[j] == '*') j++;
-        return j == n;
+        
+        if(s[i-1]==p[j-1] || p[j-1]=='?') return dp[i][j]=f(i-1,j-1,s,p,dp);
+        
+        if(p[j-1]=='*')
+        {
+            return dp[i][j] = f(i-1,j,s,p,dp) || f(i,j-1,s,p,dp) ? 1:0;
+            //Two cases
+            //Consider * as len=0
+            //Give one charcter to * and remain at *
+            //at next step it will again be decided from both these cases
+        }
+        return dp[i][j]=0;
+        
+    }
+    
+    bool isMatch(string s, string p) {
+        int n=s.length(),m=p.length();
+        vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
+        return f(n,m,s,p,dp);
     }
 };
