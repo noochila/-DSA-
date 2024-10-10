b842ea65d5dc19704bf84ed0901d928ba785bf42
Manoj N <112234009+Noochila@users.noreply.github.com>
2024-10-11 02:56:16 +0530

    Time: 509 ms (35.36%), Space: 270 MB (26.43%) - LeetHub

--- 3583-sorted-gcd-pair-queries/3583-sorted-gcd-pair-queries.cpp
+++ 3583-sorted-gcd-pair-queries/3583-sorted-gcd-pair-queries.cpp
@@ -1,47 +1,80 @@
-class Solution{
+class Solution {
 public:
-    vector<int> gcdValues(vector<int>& v, vector<long long>& q){
-        int n = v.size();
-        int maxx = *max_element(v.begin(), v.end());
-        vector<int> cntDivisors(maxx + 1, 0);
-        for(auto x : v){
-            for(int i = 1; i * i <= x; i++){
-                if(x % i == 0){
-                    cntDivisors[i]++;
-                    if(i != x / i){
-                        cntDivisors[x / i]++;
+    vector<int> gcdValues(vector<int>& nums, vector<long long>& queries) 
+    {
+         vector<long long>gcdCount(50001,0);
+
+         for(auto child:nums)
+         {
+            for(int i=1;(i*i)<=child;i++)
+            {
+                if((child%i)==0)
+                {
+                    int x=i;
+                    int y=child/i;
+                    if(x==y)
+                    {
+                        gcdCount[x]++;
+                    }
+                    else{
+                        gcdCount[x]++;
+                        gcdCount[y]++;
                     }
                 }
             }
-        }
-        vector<long long int> gcdCount(maxx + 1, 0);
-        for(int g = maxx; g >= 1; g--){
-            long long int count = cntDivisors[g];
-            gcdCount[g] = 1ll * count *(count - 1) / 2;
-            for(int mult = 2 * g; mult <= maxx; mult += g){
-                gcdCount[g] -= gcdCount[mult];
-            }
-        }
-        
-        vector<long long int> pref(maxx + 1, 0);
-        for(int g = 1; g <= maxx; g++){
-            pref[g] = pref[g - 1] + gcdCount[g];
-        }
-        vector<int> ans;
-        for(auto q : q){
-            long long int left = 1, right = maxx, answer = -1;
-            while(left <= right){
-                long long int mid =(left + right) / 2;
-                if(pref[mid] > q){
-                    answer = mid;
-                    right = mid - 1;
-                } else{
-                    left = mid + 1;
-                }
-            }
-            ans.push_back(answer);
-        }
+         }
 
-        return ans;
+        
+
+         for(int i=1;i<=50000;i++)
+         {   
+             long long value=gcdCount[i];
+             gcdCount[i]=(value*1ll*(value-1))/2ll;
+         }
+
+         
+
+         for(int i=50000;i>=0;i--)
+         {
+             if(gcdCount[i]>0)
+             {
+                    for(int j=1;(j*j)<=i;j++)
+                    {
+                        if((i%j)==0)
+                        {
+                            int x=j;
+                            int y=i/j;
+                            if(x==y && y!=i)
+                            {
+                                gcdCount[x]-=gcdCount[i];
+                            }
+                            else{
+                                if(x!=i)
+                                gcdCount[x]-=gcdCount[i];
+                                if(y!=i)
+                                gcdCount[y]-=gcdCount[i];
+                            }
+                        }
+                    }
+             }
+         }
+
+         for(int i=1;i<=50000;i++)
+         {
+            gcdCount[i]+= gcdCount[i-1];
+         }
+
+         vector<int>ans;
+
+         for(int i=0;i<queries.size();i++)
+         {
+            long long ind=queries[i]+1;
+            int myInd=lower_bound(gcdCount.begin(),gcdCount.end(),ind)-gcdCount.begin();
+            ans.push_back(myInd);
+         }
+
+         return ans;
+
+        
     }
 };
