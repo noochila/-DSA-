f25adb98fe9435dcea515dc6db4c8b02edd1d1c5
Manoj N <112234009+Noochila@users.noreply.github.com>
2024-10-16 10:57:50 +0530

    Time: 1652 ms (31.23%), Space: 61.5 MB (9.48%) - LeetHub

--- 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
+++ 0132-palindrome-partitioning-ii/0132-palindrome-partitioning-ii.cpp
@@ -1,64 +1,39 @@
-// class Solution {
-// public:
-//     bool check(string temp) {
-//         int i = 0;
-//         int j = temp.size() - 1;
-//         while (i < j) {
-//             if (temp[i] != temp[j])
-//                 return false;
-//             i++;
-//             j--;
-//         }
-//         return true;
-//     }
-
-//     int minCut(string s) {
-//         int n = s.size();
-//         vector<int> dp(n + 1, 1e9);
-//         dp[0] = -1;
-
-//         for (int i = 1; i <= n; i++) {
-//             for (int j = i; j >= 1; j--) {
-             
-//                 if (check( s.substr(j - 1, i - (j - 1)))) {
-//                     dp[i] = min(dp[i], 1 + dp[j - 1]);
-//                 }
-//             }
-//         }
-
-//         return dp[n];
-//     }
-// };
-
 class Solution {
 public:
+    // Function to check if a substring is a palindrome
+    bool isPalindrome(int i, int j, const string &s) {
+        while (i < j) {
+            if (s[i] != s[j])
+                return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+    // Helper function to find the minimum cuts using memoization
+    int solve(int i, int j, const string &s, vector<vector<int>> &dp) {
+        if (i >= j || isPalindrome(i, j, s))  // If already a palindrome or a single character
+            return 0;
+
+        if (dp[i][j] != -1)  // Check if this subproblem has already been solved
+            return dp[i][j];
+
+        int mini = INT_MAX;
+        for (int k = i; k <= j; k++) {
+            if (isPalindrome(i, k, s)) {  // Only partition if the left part is a palindrome
+                int rightPart = solve(k + 1, j, s, dp);
+                mini = min(mini, 1 + rightPart);
+            }
+        }
+
+        dp[i][j] = mini;  // Store the result in the memoization table
+        return dp[i][j];
+    }
+
     int minCut(string s) {
         int n = s.size();
-        vector<vector<bool>> isPalindrome(n, vector<bool>(n, false));
-        vector<int> dp(n, 0);
-
-        // Precompute whether substrings are palindromes
-        for (int end = 0; end < n; ++end) {
-            for (int start = 0; start <= end; ++start) {
-                if (s[start] == s[end] && (end - start <= 2 || isPalindrome[start + 1][end - 1])) {
-                    isPalindrome[start][end] = true;
-                }
-            }
-        }
-
-        for (int i = 0; i < n; ++i) {
-            if (isPalindrome[0][i]) {
-                dp[i] = 0;
-            } else {
-                dp[i] = i; // maximum cuts possible
-                for (int j = 0; j < i; ++j) {
-                    if (isPalindrome[j + 1][i]) {
-                        dp[i] = min(dp[i], dp[j] + 1);
-                    }
-                }
-            }
-        }
-
-        return dp[n - 1];
+        vector<vector<int>> dp(n, vector<int>(n, -1));  // Initialize memoization table
+        return solve(0, n - 1, s, dp);
     }
 };
