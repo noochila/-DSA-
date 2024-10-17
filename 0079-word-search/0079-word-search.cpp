e7deb39d1403fe08ca51a6090e249c6185bd3854
Manoj N <112234009+Noochila@users.noreply.github.com>
2024-10-17 20:07:24 +0530

    Time: 262 ms (72.12%), Space: 9.6 MB (98.8%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,104 +1,50 @@
 class Solution {
-    private: pair<int,int> checking(int row,int col,vector<vector<char>>&board,int count,string word)
-    {
-   
-    
-           int drow=row;
-           int dcol=col-1;
-
-           if((drow>=0 && drow<board.size())  && (dcol>=0 && dcol<board[0].size()))
-           {
-               if(word[count]==board[drow][dcol])
-               {
-                    cout<<"l";
-                     return {drow,dcol};
-                      
-               }
-             
-           }
-
-           drow=row;
-           dcol=col+1;
-
-           if((drow>=0 && drow<board.size())  && (dcol>=0 && dcol<board[0].size()))
-           {
-               if(word[count]==board[drow][dcol])
-               {
-                 cout<<"m";
-                   return {drow,dcol};
-               }
-           }
-
-           drow=row-1;
-           dcol=col;
-            if((drow>=0 && drow<board.size())  && (dcol>=0 && dcol<board[0].size()))
-           {
-               if(word[count]==board[drow][dcol])
-               {
-               cout<<"n";
-                   return {drow,dcol};
-               }
-           }
-
-           drow=row+1;
-           dcol=col;
-            if((drow>=0 && drow<board.size())  && (dcol>=0 && dcol<board[0].size()))
-           {
-               if(word[count]==board[drow][dcol])
-               {
-                cout<<"o";
-                   return {drow,dcol};
-               }
-           }
-
-       return {-1,-1};
-
-           
-        
-    }
-
-private:
-  bool check(int k,int i,int j,int n,vector<vector<char>>&board,string word)
-  {
-      if(k==n)
-      {
-          return true;
-
-      }
-          if(i<0 || j<0 || i==board.size() || j==board[0].size() || board[i][j]!=word[k])
-          return false;
-
-          char ch=board[i][j];
-          board[i][j]='#';
-
-
-          bool bool1=check(k+1,i+1,j,n,board,word);
-          bool bool2=check(k+1,i,j+1,n,board,word);
-          bool bool3=check(k+1,i-1,j,n,board,word);
-          bool bool4=check(k+1,i,j-1,n,board,word);
-              board[i][j]=ch;
-
-
-
-      return bool1 ||bool2 ||bool3||bool4;
-
-  }
-
 public:
-    bool exist(vector<vector<char>>& board, string word) {
+    vector<int> dx{-1, 0, 1, 0};
+    vector<int> dy{0, 1, 0, -1};
 
-       for(int row=0;row<board.size();row++)
-       {
-           for(int col=0;col<board[0].size();col++)
-           {
-               if(check(0,row,col,word.size(),board,word))
-               return true;
-           }
-       }
+    bool solve(vector<vector<char>>& board, string& word, int i, int j, int ind) {
+        int n = board.size();
+        int m = board[0].size();
+
+        if (ind >= word.size()) {
+            return true; 
+        }
+
+        if (i < 0 || i >= n || j < 0 || j >= m || board[i][j] != word[ind]) {
+            return false; 
+        }
+
+        
+        char temp = board[i][j];
+        board[i][j] = '#';
+
        
+        for (int k = 0; k < 4; k++) {
+            int nx = i + dx[k];
+            int ny = j + dy[k];
+            if (solve(board, word, nx, ny, ind + 1)) {
+                return true;
+            }
+        }
 
-     return false; 
+        
+        board[i][j] = temp;
+        return false;
     }
 
-    
+    bool exist(vector<vector<char>>& board, string word) {
+        int n = board.size();
+        int m = board[0].size();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (solve(board, word, i, j, 0)) {
+                    return true; 
+                }
+            }
+        }
+
+        return false; 
+    }
 };
