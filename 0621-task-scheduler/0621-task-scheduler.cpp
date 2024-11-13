9a6f960950ccca8d70cd358bf2fee9d019365c7b
Manoj N <112234009+Noochila@users.noreply.github.com>
2024-11-13 06:28:04 +0530

    Time: 40 ms (39.86%), Space: 47.6 MB (39.46%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.cpp
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -1,41 +1,41 @@
 class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
-        if (n == 0) return tasks.size();
+        // Building frequency map
+        int freq[26] = {0};
+        for (char &ch : tasks) {
+            freq[ch - 'A']++;
+        }
 
-        unordered_map<char, int> mp;
-        for (auto it : tasks) mp[it]++;
-
-        priority_queue<pair<int, char>> pq;
-        for (auto it : mp) pq.push({it.second, it.first});
-
-        int ans = 0;
-
-        while (pq.size() > 0) {
-            if (pq.top().first == 1) {
-                ans += pq.size();
-                return ans;
-            }
-            auto temp = pq.top();
-            pq.pop();
-            temp.first -= 1;
-
-            vector<pair<int, char>> arr;
-            ans++;
-            arr.push_back({temp.first, temp.second});  // Correctly store the decremented task
-
-            for (int i = 0; i < n; i++) {
-                if (pq.size() > 0) {
-                    arr.push_back({pq.top().first - 1, pq.top().second});
-                    pq.pop();
-                }
-                ans += 1;
-            }
-
-            for (auto it : arr) {
-                if (it.first > 0) pq.push(it);
+        // Max heap to store frequencies
+        priority_queue<int> pq;
+        for (int i = 0; i < 26; i++) {
+            if (freq[i] > 0) {
+                pq.push(freq[i]);
             }
         }
-        return ans;
+
+        int time = 0;
+        // Process tasks until the heap is empty
+        while (!pq.empty()) {
+            int cycle = n + 1;
+            vector<int> store;
+            int taskCount = 0;
+            // Execute tasks in each cycle
+            while (cycle-- && !pq.empty()) {
+                if (pq.top() > 1) {
+                    store.push_back(pq.top() - 1);
+                }
+                pq.pop();
+                taskCount++;
+            }
+            // Restore updated frequencies to the heap
+            for (int &x : store) {
+                pq.push(x);
+            }
+            // Add time for the completed cycle
+            time += (pq.empty() ? taskCount : n + 1);
+        }
+        return time;
     }
 };
