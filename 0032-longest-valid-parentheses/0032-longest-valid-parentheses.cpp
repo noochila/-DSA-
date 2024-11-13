3b7069672ce696b823da1d94c7b10af4718a7e91
Manoj N <112234009+Noochila@users.noreply.github.com>
2024-11-13 21:14:17 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.cpp
@@ -2,27 +2,26 @@ class Solution {
 public:
     int longestValidParentheses(string s) {
 
-        stack<int>st;
-        st.push(-1); //considering that after that index valid paranthesis starts
-        int maxi=0;
+      stack<char> st;
+      st.push(-1);
+      int maxlen=0;
 
-        for(int i=0;i<s.size();i++)
-        {
-            if(s[i]=='(')
-            st.push(i);
-            else
-            st.pop();
 
-            if(st.empty())
-            st.push(i);//consdering the string till i is invalid
-            else
-            {
-                maxi=max(maxi,i-st.top());
+      for(int i=0;i<s.size();i++)
+      {
+        if(s[i]=='(')
+        st.push(i);
+        else
+        st.pop();
 
-            }
-        }
+        if(st.empty())
+        st.push(i);
+        else
+        maxlen=max(maxlen,i-st.top());
 
-        return maxi;
+
+      }
+      return maxlen;
         
     }
 };
