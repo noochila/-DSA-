daeab81017bf9d9d9c304682cc438067a9a214a5
Manoj N <112234009+Noochila@users.noreply.github.com>
2024-11-14 12:14:49 +0530

    Time: 63 ms (40.07%), Space: 87.4 MB (22.06%) - LeetHub

---
+++ 2188-minimized-maximum-of-products-distributed-to-any-store/2188-minimized-maximum-of-products-distributed-to-any-store.cpp
@@ -0,0 +1,39 @@
+#include <vector>
+#include <numeric>
+#include <cmath>
+#include <algorithm>
+
+class Solution {
+public:
+    bool check(int k, std::vector<int>& a, int n) {
+        int stores = 0;
+
+        for (int i = 0; i < a.size(); i++) {
+            stores += std::ceil((double)a[i] / (double)k);
+
+            if (stores > n)
+                return false;
+        }
+
+        return true;
+    }
+
+    int minimizedMaximum(int n, std::vector<int>& a) {
+        long long low = 1;
+        long long high =*max_element(a.begin(),a.end());  
+        long long ans = INT_MAX;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (check(mid, a, n)) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return (long long)ans;
+    }
+};
