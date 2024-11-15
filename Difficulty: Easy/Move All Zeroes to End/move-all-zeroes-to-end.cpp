328cfe735f0579a753f75d4f9f5c40e649beb885
Manoj N <112234009+Noochila@users.noreply.github.com>
2024-11-16 00:25:16 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Move All Zeroes to End/move-all-zeroes-to-end.cpp
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+class Solution {
+  public:
+    void pushZerosToEnd(vector<int>& arr) {
+        // code here
+        int i=0;
+        int n=arr.size();
+        while(i<n)
+        {
+            if(arr[i]==0)
+            break;
+            
+            i++;
+        }
+        
+        for(int j=i+1;j<n;j++)
+        {
+            if(arr[j]!=0)
+            {
+                swap(arr[i],arr[j]);
+                i++;
+            }
+        }
+        
+        
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution ob;
+        int n = arr.size();
+        ob.pushZerosToEnd(arr);
+        for (int i = 0; i < n; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
