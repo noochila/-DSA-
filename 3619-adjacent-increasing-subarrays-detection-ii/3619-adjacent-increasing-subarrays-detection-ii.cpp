286b21adb63b5221efaf3a77015de11a4470e30b
Manoj N <112234009+Noochila@users.noreply.github.com>
2024-11-18 17:15:29 +0530

    Time: 327 ms (11.66%), Space: 290.1 MB (5.04%) - LeetHub

--- 3619-adjacent-increasing-subarrays-detection-ii/3619-adjacent-increasing-subarrays-detection-ii.cpp
+++ 3619-adjacent-increasing-subarrays-detection-ii/3619-adjacent-increasing-subarrays-detection-ii.cpp
@@ -1,24 +1,59 @@
 class Solution {
 public:
+
+
+     bool check(vector<int>& nums, int k) {
+         vector<int>prefix(nums.size());
+        
+        for(int i = 1; i < nums.size(); i++) {
+            prefix[i] = prefix[i - 1] + (nums[i] > nums[i - 1] ? 1 : 0);
+        }
+        
+        for(int i = 0; i + 2 * k - 1 < nums.size(); i++) {
+            if((prefix[i + k - 1] - prefix[i] == k - 1) && (prefix[i + 2*k - 1] - prefix[i + k] == k - 1)) {
+                return true;
+            }
+        }
+        
+        return false;
+        
+        
+    }
     int maxIncreasingSubarrays(vector<int>& nums) {
         int n = nums.size();
-        vector<int> dp(n,1);
-        for(int i=n-2;i>=0;i--){
-            if(nums[i] < nums[i+1]){
-                dp[i]+=dp[i+1];
+        // vector<int> dp(n,1);
+        // for(int i=n-2;i>=0;i--){
+        //     if(nums[i] < nums[i+1]){
+        //         dp[i]+=dp[i+1];
+        //     }
+        // }
+        // int ans = 1;
+        // int i = 0;
+        // while(i<n){
+        //     if(i+dp[i]<n){
+        //         int p = min(dp[i],dp[i+dp[i]]);
+        //         ans = max(ans,p);
+        //     }
+        //     int p = max(1,dp[i]/2);
+        //     ans = max(ans,p);
+        //     i++;
+        // }
+        // return ans;
+        int low=1;
+        int high=n;
+        while(low<=high)
+        {
+            int mid=(low+high)/2;
+
+            if(check(nums,mid))
+            {
+                low=mid+1;
+            }else
+            {
+                high=mid-1;
             }
         }
-        int ans = 1;
-        int i = 0;
-        while(i<n){
-            if(i+dp[i]<n){
-                int p = min(dp[i],dp[i+dp[i]]);
-                ans = max(ans,p);
-            }
-            int p = max(1,dp[i]/2);
-            ans = max(ans,p);
-            i++;
-        }
-        return ans;
+
+        return high;
     }
 };
