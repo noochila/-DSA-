25388c0e815fe088eacaad71b0d569e889591b2f
Manoj N <112234009+Noochila@users.noreply.github.com>
2024-11-25 19:47:05 +0530

    Time: 212 ms (25%), Space: 173.7 MB (40%) - LeetHub

--- 3648-find-the-maximum-number-of-fruits-collected/3648-find-the-maximum-number-of-fruits-collected.cpp
+++ 3648-find-the-maximum-number-of-fruits-collected/3648-find-the-maximum-number-of-fruits-collected.cpp
@@ -1,39 +1,39 @@
 class Solution {
 public:
     int solve1(int i, int j, vector<vector<int>>& fruits,
-               vector<vector<int>>& dp1,int moves) {
+               vector<vector<int>>& dp1) {
         int n = fruits.size();
-        if (i == n - 1 && j == n - 1 && moves==0)
+        if (i == n - 1 && j == n - 1)
             return 0;
 
-        if (i >= n || i < 0 || j >= n || j < 0 || i>=j || moves<0)
+        if (i >= n || i < 0 || j >= n || j < 0)
             return -1e9;
 
         if (dp1[i][j] != -1)
             return dp1[i][j];
 
-        int move1 = fruits[i][j] + solve1(i + 1, j - 1, fruits, dp1,moves-1);
-        int move2 = fruits[i][j] + solve1(i + 1, j, fruits, dp1,moves-1);
-        int move3 = fruits[i][j] + solve1(i + 1, j + 1, fruits, dp1,moves-1);
+        int move1 = fruits[i][j] + solve1(i + 1, j - 1, fruits, dp1);
+        int move2 = fruits[i][j] + solve1(i + 1, j, fruits, dp1);
+        int move3 = fruits[i][j] + solve1(i + 1, j + 1, fruits, dp1);
 
         return dp1[i][j] = max({move1, move2, move3});
     }
 
     int solve2(int i, int j, vector<vector<int>>& fruits,
-               vector<vector<int>>& dp2,int moves) {
+               vector<vector<int>>& dp2) {
         int n = fruits.size();
-        if (i == n - 1 && j == n - 1 && moves==0)
+        if (i == n - 1 && j == n - 1)
             return 0;
 
-        if (i >= n || i < 0 || j >= n || j < 0 || i<=j || moves<0 )
+        if (i >= n || i < 0 || j >= n || j < 0)
             return -1e9;
 
         if (dp2[i][j] != -1)
             return dp2[i][j];
 
-        int move1 = fruits[i][j] + solve2(i + 1, j + 1, fruits, dp2,moves-1);
-        int move2 = fruits[i][j] + solve2(i, j+1, fruits, dp2,moves-1);
-        int move3 = fruits[i][j] + solve2(i - 1, j + 1, fruits, dp2,moves-1);
+        int move1 = fruits[i][j] + solve2(i + 1, j + 1, fruits, dp2);
+        int move2 = fruits[i][j] + solve2(i, j+1, fruits, dp2);
+        int move3 = fruits[i][j] + solve2(i - 1, j + 1, fruits, dp2);
 
         return dp2[i][j] = max({move1, move2, move3});
     }
@@ -46,12 +46,11 @@ public:
         int ans1 = 0;
         for (int i = 0; i < n; i++) {
             ans1 += fruits[i][i];
-            // fruits[i][i] = 0;
+            fruits[i][i] = 0;
         }
 
-        int ans2 = solve1(0, n - 1, fruits, dp1,n-1);
-        int ans3 = solve2(n - 1, 0, fruits, dp2,n-1);
-
+        int ans2 = solve1(0, n - 1, fruits, dp1);
+        int ans3 = solve2(n - 1, 0, fruits, dp2);
 
         return ans1 + max(0, ans2) + max(0, ans3);
     }
