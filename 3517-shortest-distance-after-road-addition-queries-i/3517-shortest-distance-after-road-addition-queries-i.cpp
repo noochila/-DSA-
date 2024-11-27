33f20e019a3e8af7ce5cc9c55955f669b059e722
Manoj N <112234009+Noochila@users.noreply.github.com>
2024-11-27 10:52:03 +0530

    Time: 130 ms (65%), Space: 143.1 MB (28.94%) - LeetHub

--- 3517-shortest-distance-after-road-addition-queries-i/3517-shortest-distance-after-road-addition-queries-i.cpp
+++ 3517-shortest-distance-after-road-addition-queries-i/3517-shortest-distance-after-road-addition-queries-i.cpp
@@ -6,20 +6,19 @@ using namespace std;
 class Solution {
 public:
     vector<int> shortestDistanceAfterQueries(int n, vector<vector<int>>& queries) {
-        // Adjacency list representation
+       
         vector<vector<int>> adj(n);
         
-        // Initialize the graph with unidirectional edges from i to i + 1
         for (int i = 0; i < n - 1; i++) {
             adj[i].push_back(i + 1);
         }
         
-        // Function to calculate shortest path using BFS
+        
         auto bfs = [&](int start, int end) -> int {
             vector<int> distance(n, INT_MAX);
             queue<int> q;
             
-            // Start BFS from `start` node
+          
             q.push(start);
             distance[start] = 0;
             
@@ -40,15 +39,15 @@ public:
 
         vector<int> result;
 
-        // Process each query
+      
         for (auto& query : queries) {
             int u = query[0];
             int v = query[1];
 
-            // Add the new road
+            
             adj[u].push_back(v);
 
-            // Calculate the shortest path from city 0 to city n - 1
+            
             result.push_back(bfs(0, n - 1));
         }
 
