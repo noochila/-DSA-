00655d37397e1e46161b4e267033b6349d81b113
Manoj N <112234009+Noochila@users.noreply.github.com>
2024-12-14 09:48:33 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Search in Rotated Sorted Array/search-in-rotated-sorted-array.cpp
+++ Difficulty: Medium/Search in Rotated Sorted Array/search-in-rotated-sorted-array.cpp
@@ -6,31 +6,40 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
-    int search(vector<int>& nums, int target) {
+    int search(vector<int>& arr, int key) {
         // complete the function here
         
-        int n = nums.size();
-
-        int low = 0;
-        int high = n - 1;
-
-        while (low <= high) {
-            int mid = (low + high) / 2;
+        int low=0;
+        int high=arr.size()-1;
+        while(low<=high)
+        {
+            int mid=(low+high)/2;
             
-            if (nums[mid] == target)
+            if(arr[mid]==key)
+            {
+                
                 return mid;
-
-            if (nums[low] <= nums[mid]) {
-
-                if (nums[low] <= target && target < nums[mid])
-                    high = mid - 1;
-                else
-                    low = mid + 1;
-            } else {
-                if (nums[mid] < target && target <= nums[high])
-                    low = mid + 1;
-                else
-                    high = mid - 1;
+                
+            }
+            
+            if(arr[low]<=arr[mid])
+            {
+                if(arr[low]<=key && key<arr[mid])
+                {
+                    high=mid-1;
+                }else
+                {
+                    low=mid+1;
+                }
+            }else
+            {
+                if(key>arr[mid] && key<=arr[high])
+                {
+                    low=mid+1;
+                }else
+                {
+                    high=mid-1;
+                }
             }
         }
         return -1;
@@ -57,6 +66,7 @@ int main() {
         cin >> key;
         Solution ob;
         cout << ob.search(arr, key) << endl;
+        cout << "~" << endl;
     }
     return 0;
 }
