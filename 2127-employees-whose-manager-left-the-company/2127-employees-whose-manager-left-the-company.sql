fac30b4b2c9fa80c169790b1d787805fb5a066b7
Manoj N <112234009+noochila@users.noreply.github.com>
2024-12-15 12:20:26 +0530

    Time: 1295 ms (5.02%), Space: 0B (100%) - LeetHub

---
+++ 2127-employees-whose-manager-left-the-company/2127-employees-whose-manager-left-the-company.sql
@@ -0,0 +1,2 @@
+# Write your MySQL query statement below
+select employee_id from (select * from Employees where manager_id is not null and salary<30000 and manager_id not in (select employee_id from employees )) as temp order by employee_id
