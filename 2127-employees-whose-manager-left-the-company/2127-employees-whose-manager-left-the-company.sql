8b2de336651d524d51363e615cec0607c23874bd
Manoj N <112234009+noochila@users.noreply.github.com>
2024-12-15 12:22:09 +0530

    Time: 460 ms (17.13%), Space: 0B (100%) - LeetHub

--- 2127-employees-whose-manager-left-the-company/2127-employees-whose-manager-left-the-company.sql
+++ 2127-employees-whose-manager-left-the-company/2127-employees-whose-manager-left-the-company.sql
@@ -1,2 +1,5 @@
-# Write your MySQL query statement below
-select employee_id from (select * from Employees where manager_id is not null and salary<30000 and manager_id not in (select employee_id from employees )) as temp order by employee_id
+-- # Write your MySQL query statement below
+-- select employee_id from (select * from Employees where manager_id is not null and salary<30000 and manager_id not in (select employee_id from employees )) as temp order by employee_id
+
+
+select employee_id from employees where salary<30000 and manager_id not in (select employee_id from employees) order by employee_id
