c86181d284d9bd48ba340ef1c8b547afd00f1eaf
Manoj N <112234009+noochila@users.noreply.github.com>
2024-12-27 15:27:55 +0530

    Time: 72 ms (5.15%), Space: 72 MB (5.15%) - LeetHub

--- 1063-best-sightseeing-pair/1063-best-sightseeing-pair.cpp
+++ 1063-best-sightseeing-pair/1063-best-sightseeing-pair.cpp
@@ -1,27 +1,56 @@
 class Solution {
 public:
-    int solve(vector<int>& values, int count, int idx,      vector<vector<int>>&dp) {
-        if ( count == 0)
+    int solve(vector<int>& values, int count, int idx,
+              vector<vector<int>>& dp) {
+        if (count == 0)
             return 0;
-        if(idx>=values.size())
-        return -1e9;
+        if (idx >= values.size())
+            return -1e9;
 
-        if(dp[idx][count]!=-1)
-        return dp[idx][count];
+        if (dp[idx][count] != -1)
+            return dp[idx][count];
 
         if (count == 2) {
-            return dp[idx][count]= max(values[idx] + idx + solve(values,count - 1, idx + 1,dp),
-                       solve(values,count, idx + 1,dp));
+            return dp[idx][count] =
+                       max(values[idx] + idx +
+                               solve(values, count - 1, idx + 1, dp),
+                           solve(values, count, idx + 1, dp));
         } else {
-            return dp[idx][count]=max(values[idx] - idx + solve(values,count - 1, idx + 1,dp),
-                       solve(values,count, idx + 1,dp));
+            return dp[idx][count] =
+                       max(values[idx] - idx +
+                               solve(values, count - 1, idx + 1, dp),
+                           solve(values, count, idx + 1, dp));
         }
     }
 
     int maxScoreSightseeingPair(vector<int>& values) {
 
-        vector<vector<int>>dp(values.size()+1,vector<int>(3,-1));
+        // vector<vector<int>>dp(values.size()+1,vector<int>(3,-1));
+        vector<vector<int>> dp(values.size() + 1, vector<int>(3, -1e9));
 
-        return solve(values, 2, 0,dp);
+        // return solve(values, 2, 0,dp);
+
+        for(int i=0;i<=values.size();i++)
+        {
+            dp[i][0]=0;
+        }
+
+    
+
+        for (int idx = values.size() - 1; idx >= 0; idx--) {
+            for (int count = 2; count >= 1; count--) {
+
+                if (count == 2) {
+                    dp[idx][count] =
+                        max(values[idx] + idx + dp[idx + 1][count - 1],
+                            dp[idx + 1][count]);
+                } else {
+                    dp[idx][count] =
+                        max(values[idx] - idx + dp[idx + 1][count - 1],
+                            dp[idx + 1][count]);
+                }
+            }
+        }
+        return dp[0][2];
     }
 };
