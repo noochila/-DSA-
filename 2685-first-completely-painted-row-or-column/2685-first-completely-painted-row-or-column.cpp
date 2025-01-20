d4d1079bbef42548f29a1ba587791cc6329289f3
Manoj N <112234009+noochila@users.noreply.github.com>
2025-01-20 10:58:05 +0530

    Time: 241 ms (11.58%), Space: 164.3 MB (65.55%) - LeetHub

--- 2685-first-completely-painted-row-or-column/2685-first-completely-painted-row-or-column.cpp
+++ 2685-first-completely-painted-row-or-column/2685-first-completely-painted-row-or-column.cpp
@@ -2,34 +2,41 @@ class Solution {
 public:
     int firstCompleteIndex(vector<int>& arr, vector<vector<int>>& mat) {
 
-        int n=mat.size();
-        int m=mat[0].size();
-
         map<int,int>mp;
 
         for(int i=0;i<arr.size();i++)
         mp[arr[i]]=i;
 
-
         int ans=1e9;
 
-        for(int i=0;i<n;i++)
+        for(int i=0;i<mat.size();i++)
         {
-            int c=0;
-            for(int j=0;j<m;j++)
-            c=max(c,mp[mat[i][j]]);
+            int maxi=0;
+            for(int j=0;j<mat[0].size();j++)
+            {
+                maxi=max(maxi,mp[mat[i][j]]);
 
-            ans=min(ans,c);
+            }
+            ans=min(ans,maxi);
+
+
         }
-        for(int i=0;i<m;i++)
-        {
-            int c=0;
-            for(int j=0;j<n;j++)
-            c=max(c,mp[mat[j][i]]);
 
-            ans=min(ans,c);
+         for(int i=0;i<mat[0].size();i++)
+        {
+            int maxi=0;
+            for(int j=0;j<mat.size();j++)
+            {
+                maxi=max(maxi,mp[mat[j][i]]);
+
+            }
+            ans=min(ans,maxi);
+
+
         }
         return ans;
+
+        
         
     }
 };
