f74d668b1ada8dafc2170cb6efecb0e1c0b6fceb
Manoj N <112234009+noochila@users.noreply.github.com>
2025-05-13 23:43:57 +0530

    Time: 963 ms (6.41%), Space: 467 MB (40.39%) - LeetHub

--- 3629-total-characters-in-string-after-transformations-i/3629-total-characters-in-string-after-transformations-i.cpp
+++ 3629-total-characters-in-string-after-transformations-i/3629-total-characters-in-string-after-transformations-i.cpp
@@ -1,31 +1,30 @@
 class Solution {
 public:
-    int mod = 7 + 1e9;
-
     int lengthAfterTransformations(string s, int t) {
+        const int MOD = 1e9 + 7;
+        long long len = s.length();
 
-        vector<int> mp(26, 0);
-        for (auto it : s) {
-            mp[it - 'a']++;
+        vector<long long> freq(26, 0);
+        for(char ch : s) {
+            freq[ch - 'a']++;
         }
 
-        for (int i = 0; i < t; i++) {
-        vector<int> temp(26, 0);
-            for (int j= 0; j < 25; j++) {
-                temp[j + 1] = (temp[j + 1] + mp[j]) % mod;
+        for(int step = 1; step <= t; step++) {
+            vector<long long> newFreq(26, 0);
+            len = 0;
+            for(int i = 0; i < 26; i++) {
+                if(i == 25) { // 'z'
+                    newFreq[0] = (newFreq[0] + freq[25]) % MOD; // 'a'
+                    newFreq[1] = (newFreq[1] + freq[25]) % MOD; // 'b'
+                    len = (len + 2 * freq[25]) % MOD;
+                } else {
+                    newFreq[i + 1] = (newFreq[i + 1] + freq[i]) % MOD;
+                    len = (len + freq[i]) % MOD;
+                }
             }
-
-            temp[0] = (temp[0] + mp[25]) % mod;
-            temp[1] = (temp[1] + mp[25]) % mod;
-
-            mp = temp;
+            freq = newFreq;
         }
 
-        int ans = 0;
-        for (int i = 0; i < 26; i++) {
-            ans = (ans + mp[i]) % mod;
-            cout<<mp[i]<<endl;
-        }
-        return ans;
+        return len % MOD;
     }
 };
